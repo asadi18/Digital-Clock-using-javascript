e9ca96d2017c1457b1ffeb7cc4fe737e9d81ce7b
asadi18 <asadishteaque@gmail.com>
2023-01-07 19:09:07 +0600

    This is a digital clock using javasciprt, css and html

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function updateClock() {
     }
 
 
-    h= h < 10? "0"+h : h;
-    m= m < 10? "0"+m : m;
-    s= s < 10? "0"+s : s;
+    h = h < 10 ? "0" + h : h;
+    m = m < 10 ? "0" + m : m;
+    s = s < 10 ? "0" + s : s;
 
     hoursEl.innerText = h;
     minutesEl.innerText = m;
